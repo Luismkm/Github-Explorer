8749aae47a6caa7d6f1c36f86e384b58e78a6bf3
Luismkm <luismarcelo13@yahoo.com.br>
2021-03-14 10:14:01 -0300

    add rules and initial api

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,35 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
 
+// import api from '../../services';
 import logoImg from '../../assets/logo.svg';
 import { Title, Form, Repositories } from './styles';
 
-const Dashboard: React.FC = () => (
-  <>
-    <img src={logoImg} alt="Github Explorer" />
-    <Title>Explore repositórios no Github</Title>
+const Dashboard: React.FC = () => {
+  const [newRepo, setNewRepo] = useState('');
+  const [repositories, setRepositories] = useState([]);
 
-    <Form>
-      <input placeholder="Digite o nome do repositório" />
-      <button type="submit">Pesquisar</button>
-    </Form>
+  function handleAddRepository() {}
 
-    <Repositories>
-      <a href="teste">
-        <img
-          src="https://avatars.githubusercontent.com/u/66795134?s=460&v=4"
-          alt="Luis"
-        />
+  return (
+    <>
+      <img src={logoImg} alt="Github Explorer" />
+      <Title>Explore repositórios no Github</Title>
 
-        <div>
-          <strong>Lorem inpsum...</strong>
-          <p>Lorem inpsum...</p>
-        </div>
+      <Form>
+        <input placeholder="Digite o nome do repositório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
 
-        <FiChevronRight size={20} />
-      </a>
-    </Repositories>
-  </>
-);
+      <Repositories>
+        <a href="teste">
+          <img
+            src="https://avatars.githubusercontent.com/u/66795134?s=460&v=4"
+            alt="Luis"
+          />
+
+          <div>
+            <strong>Lorem inpsum...</strong>
+            <p>Lorem inpsum...</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  );
+};
 
 export default Dashboard;
